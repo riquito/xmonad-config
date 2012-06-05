dae02f24de35c889a8dc6b68bc6c8db397515345
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-06-05 02:38:49 +0200

    xmobar visualize a multiprocessor's cpu info

--- xmobar.hs
+++ xmobar.hs
@@ -10,15 +10,18 @@ Config {
     position = Static { xpos = 1680, ypos = 0, width = 1560, height = 16 },
     lowerOnStart = True,
     commands = [
-        Run Weather "KNYC" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
-        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Weather "LIML" ["-t","<tempC>° <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run MultiCpu ["-t","Cpu: <total>%","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","2"] 10,
+        Run CpuFreq ["-t", "Freq: <cpu0>GHz", "-L", "0", "-H", "2", "-l", "lightblue", "-n","white", "-h", "red"] 50,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run CoreTemp ["-t","Temp:<core0>C","-L","40","-H","60","-l","lightblue","-n","gray90","-h","red"] 50,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run Com "/bin/date" ["'+%d %b %H:%M'"] "localDate" 600,
+        --Run Volume "default" "Master" [] 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>   %KNYC%"
+    template = "%StdinReader% }{ %multicpu% %cpufreq% %coretemp%  %memory%   %swap%   %eth0%   <fc=#FFFFCC>%localDate%</fc>  %LIML%"
 }
