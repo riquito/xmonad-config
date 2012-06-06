af14dd6ee85bdfb4268551d512eb9571d2dd634b
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-06-06 02:31:54 +0200

    Minor xmobar layout editing

--- xmobar.hs
+++ xmobar.hs
@@ -12,9 +12,9 @@ Config {
     commands = [
         Run Weather "LIML" ["-t","<tempC>° <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
         Run MultiCpu ["-t","Cpu: <total>%","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","2"] 10,
-        Run CpuFreq ["-t", "Freq: <cpu0>GHz", "-L", "0", "-H", "2", "-l", "lightblue", "-n","white", "-h", "red"] 50,
+        Run CpuFreq ["-t", "Freq: <cpu0>", "-L", "0", "-H", "2", "-l", "lightblue", "-n","white", "-h", "red"] 50,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run CoreTemp ["-t","Temp:<core0>C","-L","40","-H","60","-l","lightblue","-n","gray90","-h","red"] 50,
+        Run CoreTemp ["-t","at <core0>C","-L","40","-H","60","-l","lightblue","-n","gray90","-h","red"] 50,
         Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run Com "/bin/date" ["'+%d %b %H:%M'"] "localDate" 600,
@@ -23,5 +23,5 @@ Config {
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu% %cpufreq% %coretemp%  %memory%   %swap%   %eth0%   <fc=#FFFFCC>%localDate%</fc>  %LIML%"
+    template = "%StdinReader% }{ %multicpu% %cpufreq% %coretemp%   %memory%  %swap%   %eth0%   <fc=#FFFFCC>%localDate%</fc>  %LIML%"
 }
